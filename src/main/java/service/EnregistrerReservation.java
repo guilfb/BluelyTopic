85a890eacf8962fba295cd8e5422ef5c2e47ae93
Moussa Ari <ari.moussa@yahoo.com>
2019-06-05 11:34:05 +0200

    Fix persist or merge reservation

--- src/main/java/service/EnregistrerReservation.java
+++ src/main/java/service/EnregistrerReservation.java
@@ -5,6 +5,9 @@ import javax.persistence.*;
 import metier.InscriptionEntity;
 import metier.ReservationEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnregistrerReservation {
 
   // on décvlare un EntityManager
@@ -24,13 +27,27 @@ public class EnregistrerReservation {
                 // On démarre une transaction
                 entityManager.getTransaction().begin();
                 // On recherche si la reservation existe  existe deja
-                InscriptionEntity reserv = entityManager.find(InscriptionEntity.class,uneR.getVehicule());
+                //ReservationEntity reserv = entityManager.find(ReservationEntity.class,uneR.getVehicule());
+                ReservationEntity reserv = new ReservationEntity();
+                List<ReservationEntity> reservList = new ArrayList<ReservationEntity>();
 
+                reservList = (List<ReservationEntity>)entityManager.createQuery(String.format("SELECT r FROM ReservationEntity r  WHERE r.vehicule=%d AND r.client= %d AND r.dateReservation=%s", uneR.getVehicule(), uneR.getClient(), uneR.getDateReservation())
+                ).getResultList();
+
+
+                if( reservList.size() == 0 )
+                    entityManager.persist(uneR);
+                else
+                    entityManager.merge(uneR);
+
+                /*
                 if( reserv==null ){
                     entityManager.persist(uneR);
                 }
                 else
                     entityManager.merge(uneR);
+
+                */
 
                 entityManager.persist(uneR);
                 entityManager.flush();
